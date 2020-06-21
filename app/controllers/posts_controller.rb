929658f009ba63cb597413575e1cd2751ac97742
YourName <you@example.com>
2020-06-22 00:29:41 +0900

    update devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -38,7 +38,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:title, :content)
+    params.require(:post).permit(:title, :content).merge(user_id: current_user.id)
   end
 
 end
