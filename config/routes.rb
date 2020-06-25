0990c3ef6b253e286d95434735fcd6813f6e32f4
YourName <tacccccc21@gmail.com>
2020-06-25 18:22:24 +0900

    create_message_model

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 root to: 'posts#index'
 resources :posts
 resources :users, only: [:show, :index, :destroy]
-resources :groups, only: [:index, :new, :create, :edit, :update]
+resources :groups, only: [:index, :new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
